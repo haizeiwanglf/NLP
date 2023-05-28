294ff0efc73b9d0e8eb143a90ba9acd81497116b
wushuo <1912002436@qq.com>
2023-05-28 20:42:03 +0800

    wandb

--- bert-classification/README.md
+++ bert-classification/README.md
@@ -1,12 +1,24 @@
 # wandb使用
 清华大学数据集文本分类。
 
-eval:
+对比
 
-![image-20230528134738289](C:\Users\wushuo\AppData\Roaming\Typora\typora-user-images\image-20230528134738289.png)
+|      | base      | lora                 | prompt-tuning | p-tuning | p-tuning2 | prefix-tuning |
+| ---- | --------- | -------------------- | ------------- | -------- | --------- | ------------- |
+| acc  |           | 91.14                |               |          |           |               |
+| p    |           | 91.15                |               |          |           |               |
+| r    |           | 91.11                |               |          |           |               |
+| f1   |           | 91.11                |               |          |           |               |
+| 显存 | 4.3G      | 2.7G(后面上升到3.5G) |               |          |           |               |
+| 时间 | 42.14分钟 | 30.01分钟            |               |          |           |               |
+| 参数 | bert 1亿  | 362602（29.5%）      |               |          |           |               |
 
+损失函数:
 
+base
 
-train:
+![image-20230528201632118](C:\Users\wushuo\AppData\Roaming\Typora\typora-user-images\image-20230528201632118.png)
 
-![image-20230528134909549](C:\Users\wushuo\AppData\Roaming\Typora\typora-user-images\image-20230528134909549.png)
+lora
+
+![image-20230528203301648](C:\Users\wushuo\AppData\Roaming\Typora\typora-user-images\image-20230528203301648.png)
--- bert-classification/lora_finetune.py
+++ bert-classification/lora_finetune.py
@@ -1,30 +1,66 @@
 import random
-from transformers import AutoConfig, AutoTokenizer,AutoModelForSequenceClassification, Trainer, TrainingArguments,set_seed
+from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments, \
+    set_seed
 from torch.utils.data import Dataset
 from transformers.trainer_utils import IntervalStrategy
 import torch
+from peft import (
+    get_peft_config,
+    get_peft_model,
+    get_peft_model_state_dict,
+    set_peft_model_state_dict,
+    PeftType,
+    PrefixTuningConfig,
+    PromptEncoderConfig,
+    PromptTuningConfig,
+    LoraConfig
+)
 
 import numpy as np
 from sklearn.metrics import precision_recall_fscore_support
 from tqdm import tqdm
+import time
 
 set_seed(42)
-model_name="bert-base-chinese"
-num_labels=10
+model_name = "bert-base-chinese"
+num_labels = 10
 
-model_config = AutoConfig.from_pretrained(model_name,num_labels=num_labels)
+if any(k in model_name for k in ("gpt", "opt", "bloom")):
+    padding_side = "left"
+else:
+    padding_side = "right"
+model_config = AutoConfig.from_pretrained(model_name, num_labels=num_labels)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=10)
+# 训练器配置
+p_type = "lora"
+if p_type == "prefix-tuning":
+    peft_type = PeftType.PREFIX_TUNING
+    peft_config = PrefixTuningConfig(task_type="SEQ_CLS", num_virtual_tokens=20)
+elif p_type == "prompt-tuning":
+    peft_type = PeftType.PROMPT_TUNING
+    peft_config = PromptTuningConfig(task_type="SEQ_CLS", num_virtual_tokens=20)
+elif p_type == "p-tuning":
+    peft_type = PeftType.P_TUNING
+    peft_config = PromptEncoderConfig(task_type="SEQ_CLS", num_virtual_tokens=20, encoder_hidden_size=128)
+elif p_type == "lora":
+    peft_type = PeftType.LORA
+    peft_config = LoraConfig(task_type="SEQ_CLS", inference_mode=False, r=8, lora_alpha=16, lora_dropout=0.1)
+
+if p_type is not None:
+    model = get_peft_model(model, peft_config)
+    model.print_trainable_parameters()
+
 
 def load_data(path):
     with open(path, 'r', encoding='UTF-8') as f:
-        data=[]
+        data = []
         for line in tqdm(f):
             lin = line.strip()
             if not lin:
                 continue
             content, label = lin.split('\t')
-            data.append((content,int(label)))
+            data.append((content, int(label)))
     random.shuffle(data)
     return data
 
@@ -43,17 +79,17 @@ class MyDataset(Dataset):
         return [text, label]
 
 def collate_fn(features):
-    text=[line[0] for line in features]
-    label=[line[1] for line in features]
-    tokenized = tokenizer(text,padding=True,return_tensors='pt')
+    text = [line[0] for line in features]
+    label = [line[1] for line in features]
+    tokenized = tokenizer(text, padding=True, return_tensors='pt')
     input_ids = tokenized['input_ids']
     attention_mask = tokenized['attention_mask']
     labels = torch.tensor(label)
     return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels}
 
-train_data=load_data("./data/train.txt")
-dev_data=load_data("./data/dev.txt")
-test_data=load_data("./data/test.txt")
+train_data = load_data("./data/train.txt")
+dev_data = load_data("./data/dev.txt")
+test_data = load_data("./data/test.txt")
 
 train = MyDataset(train_data)
 valid = MyDataset(dev_data)
@@ -63,12 +99,14 @@ def compute_metrics(eval_preds):
     logits, labels = eval_preds
     preds = np.argmax(logits, axis=-1)
     precision, recall, f1, _ = precision_recall_fscore_support(labels, preds.flatten(), average='macro')
+    print({'accuracy': round((labels == preds).mean(), 6),
+           'f1': round(f1, 6),
+           'precision': round(precision, 6),
+           'recall': round(recall, 6)})
     return {'accuracy': round((labels == preds).mean(), 6),
             'f1': round(f1, 6),
             'precision': round(precision, 6),
-            'recall': round(recall, 6),
-            "our":1}
-
+            'recall': round(recall, 6)}
 
 training_args=TrainingArguments(output_dir='./weights',
                                     do_train=True,
@@ -95,5 +133,32 @@ trainer = Trainer(model=model,
                   compute_metrics=compute_metrics
                   )
 
+start = time.time()
 trainer.train()
-print("fin")
+model.save_pretrained(p_type)
+end = time.time()
+print("训练耗时：{}分钟".format((end - start) / 60))
+
+res=trainer.predict(test)
+print(res.metrics)
+print("finish")
+
+#inference
+# from peft import PeftModel
+# import torch
+# from transformers import AutoModelForSequenceClassification,AutoTokenizer
+# model_name = "bert-base-chinese"
+# lora_model_dir = "./lora"
+# model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=10)
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# lora_model = PeftModel.from_pretrained(model, lora_model_dir)
+# lora_model.cuda()
+# lora_model.eval()
+# text = '中国人民公安大学2012年硕士研究生目录及书目'
+# with torch.no_grad():
+#   inputs = tokenizer(text, truncation=True, max_length=86, padding="max_length", return_tensors="pt")
+#   for k, v in inputs.items():
+#     inputs[k] = v.cuda()
+#   output = lora_model(**inputs)
+#   logits = output.logits
+#   print(logits)
