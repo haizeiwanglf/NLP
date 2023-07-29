6f379888c24bc9da7661d5b0863c49d3fa42d1b7
wushuo <1912002436@qq.com>
2023-07-29 23:55:10 +0800

    glm

--- chatglm/README.md
+++ chatglm/README.md
@@ -1,5 +1,135 @@
-# chatglm项目集合
+# chatglm_tuning: 基于 LoRA 和 P-Tuning v2 的 ChatGLM-6B 高效参数微调
+
+[ChatGLM-6B](https://github.com/THUDM/ChatGLM-6B) 是一个清华开源的、支持中英双语的对话语言模型，基于 [General Language Model (GLM)](https://github.com/THUDM/GLM) 架构，具有 62 亿参数。经过约 1T 标识符的中英双语训练，辅以监督微调、反馈自助、人类反馈强化学习等技术的加持，62 亿参数的 ChatGLM-6B 已经能生成相当符合人类偏好的回答。
+
+本仓库实现了对于 ChatGLM-6B 模型基于 LoRA 和 P-Tuning v2 的参数高效微调。
+
+
+
+## Requirements
+
+- transformers==4.30
+
+
+## P-Tuning v2
+
+[P-Tuning v2](https://github.com/THUDM/P-tuning-v2) 是清华大学开源的语言模型提示微调方法，在 3 亿到 100 亿参数的广阔参数规模上，均能仅以 0.1%～3% 的微调参数量，取得和精调方法媲美的迁移效果。
+
+下面以huatuo医疗模型的数据集为实例进行微调
+
+```json
+{
+    "instruction": "颜面部凹陷的手术治疗有些什么？", 
+    "output": "自体颗粒脂肪移植；自体脂肪移植；自体脂肪干细胞移植；自体脂肪颗粒移植"
+}
+```
+
+对原始数据进行预处理，改变其列名:
+
+```shell
+cd dataset/FreedomIntelligencehuatuo_knowledge_graph_qa/
+python convert_to_instruct.py
+```
+
+运行以下命令进行训练：
+
+```shell
+export CUDA_VISIBLE_DEVICES = 0  train_ptuning.py \
+--train_args_file ./config/chatglm_6b_ptuning.json \
+--data_path ./dataset/FreedomIntelligencehuatuo_knowledge_graph_qa/train_huatuo.json \
+--pre_seq_len 128 \
+
+```
+
+如果想进行单机多卡分布式训练，可运行如下命令：
+
+```shell
+export CUDA_VISIBLE_DEVICES = 0, 1, 2, 3
+torchrun --nproc_per_node=4  train_ptuning.py \
+--train_args_file ./config/chatglm_6b_ptuning.json \
+--data_path ./dataset/FreedomIntelligencehuatuo_knowledge_graph_qa/train_huatuo.json \
+--pre_seq_len 128 \
+
+```
+
+chatglm_6b_ptuning.json 为训练参数配置文件：
+
+```json
+{
+    "output_dir": "output/adgen-chatglm-6b-ptuning",
+    "per_device_train_batch_size": 4,
+    "gradient_accumulation_steps": 4,
+    "learning_rate": 2e-2,
+    "num_train_epochs": 1.0,
+    "max_steps": 3000,
+    "lr_scheduler_type": "cosine",
+    "logging_steps": 5,
+    "save_strategy": "no",
+    "optim": "adamw_torch",
+    "fp16": false,
+    "remove_unused_columns": false,
+    "ddp_find_unused_parameters": false,
+    "report_to": "tensorboard"
+}
+```
+
+在json中，max_steps与num_train_epochs参数分别控制最大步数和训练epoch数目，当两者冲突时优先选择前者，如果num_train_epochs为1，那么训练的步数为 num_sample/(per_device_train_batch_size*device_num+gradient_accumulation_steps)
+
+在 P-tuning v2 训练时模型只保存 PrefixEncoder 部分的参数，所以在推理时需要同时加载原 ChatGLM-6B 模型以及 PrefixEncoder 的权重。
+
+```shell
+
+```
+
+
+
+## LoRA
+
+[LoRA](https://github.com/microsoft/LoRA) 的实现思想很简单，如下图所示，就是冻结一个预训练语言模型的矩阵参数，并选择用 `A` 和 `B` 矩阵来替代，在下游任务时只更新 `A` 和 `B`。
+
+![](E:\chatglm_tuning-main\images\lora.png)
+
+ LoRA 的实现流程概况如下：
+
+- 在原始预训练语言模型 (PLM) 旁增加一个旁路，做一个先降维再升维的操作，以此来模拟所谓的内在秩；
+
+- 训练的时候固定 PLM 的参数不变，只训练降维矩阵 `A` 和升维矩阵 `B`，即优化器只优化右路的参数；
+
+- 模型的输入、输出维度不变，左右两边共用模型的输入，输出时将 PLM 与 `A-B` 的输出叠加；
+
+- 用随机高斯分布 $N(0,\sigma^2)$ 初始化 `A`，用全零矩阵初始化 `B`。矩阵 `B` 的全零初始化，使得在训练最开始的一段时间，右路的结果会接近于0，这样模块的输出就基本上来自于左路，也就是大模型原有参数的计算结果，这使得模型优化的初始点就和原始的大模型保持一致。
+
+
+
+运行如下命令进行单机多卡分布式训练：
+
+```shell
+export CUDA_VISIBLE_DEVICES = 0, 1, 2, 3
+torchrun --nproc_per_node=4  train_lora.py \
+--model_name_or_path /data/wuguangshuo/THUDMchatglm-6b/ \
+--train_args_file ./config/chatglm_6b_lora.json \
+--data_path ./dataset/FreedomIntelligencehuatuo_knowledge_graph_qa/train_huatuo.json \
+--max_input_length 256 \
+--max_output_length 256
+```
+
+训练完成后，运行如下命令进行推理：
+
+```shell
+python cli_demo.py \
+--model_name_or_path /path/to/chatglm-6b/ \
+--lora_checkpoint output/adgen-chatglm-6b-lora/ \
+--no_history
+```
+
+
+
+
+
+
+
 1. https://github.com/THUDM/ChatGLM-6B
+1. [zejunwang1/chatglm_tuning: 基于 LoRA 和 P-Tuning v2 的 ChatGLM-6B 高效参数微调 (github.com)](https://github.com/zejunwang1/chatglm_tuning)(主要参考对象)
 2. https://github.com/liucongg/ChatGLM-Finetuning
 3. https://github.com/HarderThenHarder/transformers_tasks/tree/main/LLM
 4. https://github.com/hiyouga/ChatGLM-Efficient-Tuning
--- chatglm/train_ptuning.py
+++ chatglm/train_ptuning.py
@@ -3,6 +3,7 @@ import os
 import torch
 from datasets import load_dataset
 from transformers import (
+    AutoConfig,
     AutoModel,
     AutoTokenizer,
     HfArgumentParser,
@@ -10,36 +11,30 @@ from transformers import (
     TrainingArguments,
     Trainer
 )
-from peft import (
-    TaskType,
-    LoraConfig,
-    get_peft_model,
-    set_peft_model_state_dict,
-    prepare_model_for_int8_training
-)
-from peft.utils import TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="ChatGLM LoRA")
     parser.add_argument("--train_args_file", type=str, default=None)
-    parser.add_argument("--model_name_or_path", type=str, default=None)
+    parser.add_argument("--model_name_or_path", type=str, default="/data/wuguangshuo/THUDMchatglm-6b/")
     parser.add_argument("--data_path", type=str, required=True)
     parser.add_argument("--eval_path", type=str, default=None)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--max_input_length", type=int, default=256)
     parser.add_argument("--max_output_length", type=int, default=256)
-    parser.add_argument("--lora_rank", type=int, default=8)
-    parser.add_argument("--lora_alpha", type=int, default=32)
-    parser.add_argument("--lora_dropout", type=float, default=0.05)
-    parser.add_argument("--overwrite_cache", type=bool, help="是否加载之前处理过的数据,True的话加载",default=True)
+    parser.add_argument("--overwrite_cache", type=bool, default=True)
     parser.add_argument("--resume_from_checkpoint", type=str, default=None)
     # parser.add_argument("--resume_from_checkpoint", type=str, default="/home/wuguangshuo/dev_env/chatglm_tuning-main/output/adgen-chatglm-6b-lora/")
     parser.add_argument("--quantization_bit", type=int, choices=[4, 8], default=None)
     parser.add_argument("--model_parallel", type=bool, default=True)
     parser.add_argument("--no_gradient_checkpointing", type=bool, default=False)
+    parser.add_argument("--pre_seq_len", type=int, default=None)
+    parser.add_argument("--prefix_projection", action="store_true")
+    parser.add_argument("--ptuning_checkpoint", type=str, default=None)
     args = parser.parse_args()
     return args
+
+
 
 class DataCollator:
     def __init__(self, pad_token_id):
@@ -62,81 +57,69 @@ class DataCollator:
 
 
 class ModifiedTrainer(Trainer):
-    def save_model(self, output_dir=None, _internal_call=False):
-        if output_dir is None:
-            output_dir = self.args.output_dir
-        self.model.save_pretrained(output_dir)
+    def _save(self, output_dir=None, state_dict=None):
+        # If we are executing this function, we are the process zero, so we don't check for that.
+        output_dir = output_dir if output_dir is not None else self.args.output_dir
+        os.makedirs(output_dir, exist_ok=True)
         torch.save(self.args, os.path.join(output_dir, "training_args.bin"))
+        saved_params = {
+            k: v.to("cpu") for k, v in self.model.named_parameters() if v.requires_grad
+        }
+        torch.save(saved_params, os.path.join(output_dir, "pytorch_model.bin"))
 
 
 def train(args):
     parser = HfArgumentParser(TrainingArguments)
     training_args, = parser.parse_json_file(json_file=args.train_args_file)
+
     # Distributed training
-    # if we are in a distributed setting, we need to set the device map and max memory per device
-    device_map = "auto"
     if "LOCAL_RANK" in os.environ:
         training_args.local_rank = int(os.environ["LOCAL_RANK"])
-        device_map = {"": training_args.local_rank}
 
     # Set seed
     set_seed(args.seed)
     training_args.seed = args.seed
 
     # Load model and tokenizer
+    config = AutoConfig.from_pretrained(args.model_name_or_path, trust_remote_code=True)
+    # 当prefix_projection为True时，为P - Tuning - V2方法，在大模型的Embedding和每一层前都加上新的参数；为False时，为P - Tuning方法，仅在大模型的Embedding上新的参数。
+    config.pre_seq_len = args.pre_seq_len
+    config.prefix_projection = args.prefix_projection
+
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, trust_remote_code=True)
-    model = AutoModel.from_pretrained(args.model_name_or_path, trust_remote_code=True,device_map=device_map)
-    #加载量化模型
+
+    if args.ptuning_checkpoint is not None:
+        # Loading extra state dict of prefix encoder
+        model = AutoModel.from_pretrained(args.model_name_or_path, config=config,
+                                          trust_remote_code=True)
+        prefix_state_dict = torch.load(os.path.join(args.ptuning_checkpoint,
+                                                    "pytorch_model.bin"))
+        new_prefix_state_dict = {}
+        for k, v in prefix_state_dict.items():
+            if k.startswith("transformer.prefix_encoder."):
+                new_prefix_state_dict[k[len("transformer.prefix_encoder."):]] = v
+        model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
+    else:
+        model = AutoModel.from_pretrained(args.model_name_or_path, config=config,
+                                          trust_remote_code=True)
+
     if args.quantization_bit is not None:
         print(f"Quantized to {args.quantization_bit} bit")
         model = model.quantize(args.quantization_bit)
 
-    model = model.half()
-    #梯度累计
+    if args.pre_seq_len is not None:
+        # P-tuning v2
+        model = model.half()
+        model.transformer.prefix_encoder.float()
+    else:
+        # deepspeed finetune
+        model = model.float()
+
     if not args.no_gradient_checkpointing:
         model.gradient_checkpointing_enable()
         model.enable_input_require_grads()
         model.config.use_cache = False
-    #并行化训练
-    if args.model_parallel:
-        model.is_parallelizable = True
-        model.model_parallel = True
 
-    # Define LoRA Config
-    target_modules = TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING["chatglm"]
-    lora_config = LoraConfig(
-        r=args.lora_rank,
-        lora_alpha=args.lora_alpha,
-        target_modules=target_modules,
-        lora_dropout=args.lora_dropout,
-        bias="none",
-        inference_mode=False,
-        task_type=TaskType.CAUSAL_LM
-    )
-
-    # add LoRA adaptor
-    model = get_peft_model(model, lora_config)
-
-    #是否加载之前的模型
-
-    resume_from_checkpoint = args.resume_from_checkpoint
-    if resume_from_checkpoint is not None:
-        # Full checkpoint
-        checkpoint_name = os.path.join(resume_from_checkpoint, "pytorch_model.bin")
-        if not os.path.exists(checkpoint_name):
-            checkpoint_name = os.path.join(
-                resume_from_checkpoint, "adapter_model.bin"
-            )  # only LoRA model - LoRA config above has to fit
-            resume_from_checkpoint = False  # So the trainer won't try loading its state
-        if os.path.exists(checkpoint_name):
-            print(f"Restarting from {checkpoint_name}")
-            adapters_weights = torch.load(checkpoint_name)
-            set_peft_model_state_dict(model, adapters_weights)
-            print("加载成功")
-        else:
-            print(f"Checkpoint {checkpoint_name} not found")
-
-    model.print_trainable_parameters()
     # Load dataset
     data = load_dataset(path="json", data_files=args.data_path)
     column_names = data["train"].column_names
@@ -160,26 +143,53 @@ def train(args):
         labels = [-100] * question_length + input_ids[question_length:]
         return {"input_ids": input_ids, "labels": labels}
 
-    train_dataset = data["train"].map(tokenize_function, remove_columns=column_names,load_from_cache_file=args.overwrite_cache)
+    train_dataset = data["train"].map(tokenize_function, remove_columns=column_names)
     eval_dataset = None
     if args.eval_path is not None:
         eval_data = load_dataset(path="json", data_files=args.eval_path)
         eval_dataset = eval_data["train"].map(tokenize_function, remove_columns=column_names)
 
     # trainer
-    trainer = ModifiedTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
-        data_collator=DataCollator(pad_token_id=tokenizer.pad_token_id),
-    )
+    if args.pre_seq_len is not None:
+        trainer = ModifiedTrainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            data_collator=DataCollator(pad_token_id=tokenizer.pad_token_id)
+        )
+    else:
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            data_collator=DataCollator(pad_token_id=tokenizer.pad_token_id)
+        )
 
     # train model
-    trainer.train(resume_from_checkpoint=resume_from_checkpoint)
+    trainer.train(resume_from_checkpoint=training_args.resume_from_checkpoint)
 
-    # Save our LoRA model & tokenizer results
-    trainer.model.save_pretrained(training_args.output_dir)
+    # Save model & tokenizer results
+    if args.pre_seq_len is not None:
+        print("Saving PrefixEncoder")
+        os.makedirs(training_args.output_dir, exist_ok=True)
+        saved_params = {
+            k: v.to("cpu") for k, v in trainer.model.named_parameters() if v.requires_grad
+        }
+        torch.save(saved_params, os.path.join(training_args.output_dir, "pytorch_model.bin"))
+        """
+        print("Saving PrefixEncoder")
+        state_dict = trainer.model.state_dict()
+        filtered_state_dict = {}
+        for k, v in trainer.model.named_parameters():
+            if v.requires_grad:
+                filtered_state_dict[k] = state_dict[k]
+        trainer.model.save_pretrained(training_args.output_dir, 
+            state_dict=filtered_state_dict)
+        """
+    else:
+        trainer.save_model(training_args.output_dir)
     tokenizer.save_pretrained(training_args.output_dir)
 
 
@@ -188,9 +198,7 @@ if __name__ == "__main__":
     train(args)
 
 # export CUDA_VISIBLE_DEVICES = 0, 1, 2, 3
-# torchrun --nproc_per_node=4  train_lora.py \
-# --model_name_or_path /data/wuguangshuo/THUDMchatglm-6b/ \
-# --train_args_file ./config/chatglm_6b_lora.json \
+# torchrun --nproc_per_node=4  train_ptuning.py \
+# --train_args_file ./config/chatglm_6b_ptuning.json \
 # --data_path ./dataset/FreedomIntelligencehuatuo_knowledge_graph_qa/train_huatuo.json \
-# --max_input_length 256 \
-# --max_output_length 256
+# --pre_seq_len 128 \
